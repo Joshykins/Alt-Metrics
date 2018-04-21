6a97515243afbd153dc1a45c5cf39431e1a0de81
Joshykins <joshuaroelle2@gmail.com>
2018-04-21 15:08:05 -0700

    init2

---
+++ main.js
@@ -0,0 +1,19 @@
+// Alternative Metrics
+// :) ========== (:
+// Converts all values to watts, then each 'power' has it's watt->this.power conversion within it.
+// 1 watt/s = 1 watt/s
+// watts/s
+
+// 1 hp = 745.7 watt/s
+// horsepower
+
+//1  KangarooPower = 50 Watt/s
+//Kangaroo hopping
+
+// 1 BirdPower = 65Watt/s
+// BirdOutput
+// http://ib.oregonstate.edu/~warrickd/dialetal97.pdf
+
+//
+//
+//
